c83241fdb9f98cb010f6ac7f297dfe1a542ecf5b
Brandon Richardson <brandon1024.br@gmail.com>
2018-11-17 10:39:07 -0400

    app running

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,23 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import DaySpanVuetify from 'dayspan-vuetify';
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import DaySpanVuetify from 'dayspan-vuetify'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+import 'vuetify/dist/vuetify.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
+
+Vue.config.productionTip = false
 
 Vue.use(Vuetify);
 
-Vue.use( DaySpanVuetify, {
-  // options is vue definition, the resulting reactive component is stored in components as
-  // this.$dayspan or Vue.$dayspan
-  data: {
-    // data or computed to override
-  },
-  computed: {
-    // data or computed to override
-  },
-  methods: {
-    getDefaultEventColor: () => '#1976d2',
-  },
+Vue.use(DaySpanVuetify, {
+    methods: {
+        getDefaultEventColor: () => '#1976d2'
+    }
 });
 
-
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+    el: '#app',
+    render: h => h(App)
+})
