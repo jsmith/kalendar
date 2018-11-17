1f4875160e557ce6edde01d974129f4668c8dd3e
alexodonn <alexodonn@gmail.com>
2018-11-17 10:27:14 -0400

    moved calendar logic to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import DaySpanVuetify from 'dayspan-vuetify';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
+
+Vue.use( DaySpanVuetify, {
+  // options is vue definition, the resulting reactive component is stored in components as
+  // this.$dayspan or Vue.$dayspan
+  data: {
+    // data or computed to override
+  },
+  computed: {
+    // data or computed to override
+  },
+  methods: {
+    getDefaultEventColor: () => '#1976d2',
+  },
+});
+
 
 new Vue({
   router,
