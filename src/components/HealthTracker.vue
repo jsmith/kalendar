5e453ba5b125925cccab0a07e9193ce4fef78ab8
Amir Eldesoky <aeldesok@unb.ca>
2018-11-17 13:23:28 -0400

    getting the totals for health tracker

--- src/components/HealthTracker.vue
+++ src/components/HealthTracker.vue
@@ -19,11 +19,61 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import ProgressRing from '@/components/ProgressRing.vue';
 
+interface Event {
+    data: {
+          title: string,
+          color: string,
+          calendar?: string,
+      },
+      schedule: {
+          dayOfWeek: string[],
+          times: number[],
+          duration: number,
+          durationUnit: string,
+      },
+}
+
+interface State {
+    events: Event[]
+}
+
 @Component({
     components: {ProgressRing},
 })
 export default class HealthTracker extends Vue {
+  @Prop({type: Object}) public state!: State;
 
+    get physicalActivitesTotal() {
+        let totalTime = 0;
+        let scaleFactor = 1;
+
+        this.state.events.forEach(event => {
+            if (event.data.calendar === 'Physical Activities') {
+                scaleFactor = event.schedule.durationUnit === 'days' ? 24:
+                              event.schedule.durationUnit ===  'hours' ? 60: 1;
+            }
+
+            totalTime += event.schedule.duration * scaleFactor;
+        });
+
+        return totalTime;
+    }
+
+    get eatingTotal() {
+        let totalTime = 0;
+        let scaleFactor = 1;
+
+        this.state.events.forEach(event => {
+            if (event.data.calendar === 'Eating') {
+                scaleFactor = event.schedule.durationUnit === 'days' ? 24:
+                              event.schedule.durationUnit ===  'hours' ? 60: 1;
+            }
+
+            totalTime += event.schedule.duration * scaleFactor;
+        });
+
+        return totalTime;
+    }
 }
 </script>
 
